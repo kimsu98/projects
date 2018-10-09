8e02181530f39f7b3634cd9c950b37169cf373d4
boluv <boluv@192.168.0.15>
2018-10-09 23:11:36 +0900

    initial check-in

---
+++ demo1/src/test/java/com/example/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
